334eee8772be72075ec316530b3febdef329ba61
Siddharth Akar <57083596+Sid1608@users.noreply.github.com>
2020-11-08 14:30:36 +0530

    Update Patients.java

--- src/EHMS/Patients.java
+++ src/EHMS/Patients.java
@@ -6,8 +6,57 @@ import java.sql.*;
 import java.util.Scanner;
 public class Patients extends Person
 {
+	private int PatientID;
 	Scanner sc=new Scanner(System.in);
     String BloodGroup ;
+    private void generatePatientID()
+	{
+		
+		try
+		{
+			Connection con=ConnectionProvider.getCon();
+			Statement st=con.createStatement();
+		
+			ResultSet rs=st.executeQuery("Select MAX(UserID) from Users where userType is Patient");
+			rs.next();
+			rs.getInt("MAX(UserID)");
+//			if(rs.getInt("MAX(UserID)"))
+//				return 1;
+//			else
+//				return id+1;
+				
+			
+		}catch(Exception e)
+		{
+			
+		}
+	}
+    public void addPatient() 
+	{
+		PatientID=generatePatientID();
+		String password;
+		String cpd;
+		System.out.println("Patient ID:"+PatientID);
+		System.out.println("Enter Password:");
+		password=sc.next();
+		while(true)
+		{
+			System.out.println("Confirm Password");
+			cpd=sc.next();
+			if(password.compareTo(cpd)==0)
+					break;
+		}
+		try {
+			Connection con=ConnectionProvider.getCon();
+			Statement st=con.createStatement();
+			st.executeUpdate("insert into Users values('"+PatientID+"','"+password+"','"+"Patient"+"')");
+			System.out.println("Registered Succesfully!!");
+		}catch(Exception e){
+			System.out.println("Please enter data in correct format!!");
+		}
+		//return PatientID;
+		
+	}
     public void PatientRegistration(int pid) 
     {
   
@@ -15,7 +64,7 @@ public class Patients extends Person
     	System.out.println("BloodGroup:");
     	BloodGroup=sc.next();
     	Register reg=new Register();
-    	reg.patient_Registration(pid,First_Name,Last_Name,Gender,CN,age,Email_Address);
+    	reg.patient_Registration(pid,First_Name,Last_Name,Gender,CN,age,Email_Address,BloodGroup);
  
     }
     public void ShowPatientDetails(int id)
