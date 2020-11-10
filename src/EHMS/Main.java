64ab71fb2e2a1e38b1b607be10d5b61986d7b7b9
Siddharth Akar <57083596+Sid1608@users.noreply.github.com>
2020-11-10 14:50:02 +0530

    Update Main.java

--- src/EHMS/Main.java
+++ src/EHMS/Main.java
@@ -3,11 +3,11 @@ import EHMS.ConnectionProvider;
 import java.sql.*;
 import java.util.Scanner;
 
-public class Main
+public class Main 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		System.out.println("******************************E-HealthCare-Management-Sytem**********************************");
+		System.out.println("*************E-HealthCare-Management-Sytem*************");
 		Scanner sc=new Scanner(System.in);
 		Admin a=new Admin();
 		Patients p=null;
@@ -16,15 +16,15 @@ public class Main
 		Appointment[] doc_app=new Appointment[10000];
 		Patients[] P=new Patients[10000];
 		Doctor[] D=new Doctor[10000];
-		while(true)
+		while(true)//this is to inform you that 
 		{
-		System.out.println("1.AdminLogin\n 2.DoctorLogin.\n3.PatientLogin \n");
+		System.out.println("1.AdminLogin\n 2.PatientLogin.\n3.DoctorLogin \n4.Patient-SignUp");		
 		int choice = sc.nextInt();
 		switch (choice)
 		{
 		    case 1:  // admins portal
-		    {
-		    	System.out.println("***********Welcome to Admins portal******************");
+		    {   
+		    	System.out.println("************Welcome to Admins portal************");
 		    	String un;
 		    	String pd;
 		    	System.out.print("USERNAME-->");un=sc.next();
@@ -33,7 +33,7 @@ public class Main
 		    	{
 		    		while(true)
 		    		{
-		    			System.out.println("1.ViewDoctors\n2.ViewPatients.\n3.AddPatient\n4.AddDoctor\n5.RemovePatient\n6.RemoveDoctor\n7.viewAppointments\n8.viewPaymentDetils\n9.updateDoctorsdetails\n10.UpdatePatientDetails");
+		    			System.out.println("1.viewDoctors\n2.viewPatients.\n3.addDoctor\n4.removePatient\n5.RemoveDoctor\n6.viewAppointments\n7.viewPaymentDetils\n8.updateDoctorsdetails\n9.UpdatePatientDetails");
 		    			int ch=sc.nextInt();
 		    			switch(ch)
 		    			{
@@ -49,45 +49,38 @@ public class Main
 		    				}
 		    				case 3:
 		    				{
-		    					int id=a.addPatient();
-		    			    	//p=new Patients();
-		    					//p.PatientRegistration(id);
-		    					break;
-		    				}
-		    				case 4:
-		    				{
 		    					int Id=a.addDoctor();
 		    					d=new Doctor();
 		    					d.DoctorRegistration(Id);
 		    					break;
 		    				}
-		    				case 5:
+		    				case 4:
 		    				{
-		    					System.out.println("Enter doctorID!!");
+		    					System.out.println("Enter doctor_ID!!");
 		    					int id=sc.nextInt();
 		    					a.RemoveDoctor(id);
 		    					break;
 		    				}
-		    				case 6:
+		    				case 5:
 		    				{
-		    					System.out.println("Enter doctorID!!");
+		    					System.out.println("Enter Patient_ID!!");
 		    					int id=sc.nextInt();
 		    					a.RemovePatient(id);
 		    					break;
 		    				}
-		    				case 7:
+		    				case 6:
 		    				{
 		    					break;
 		    				}
 		    			}
 		    		}
-
+		    		
 		    	}
 		    	else
 		    		System.out.println("Invalid username or password");
 		    	break;
 		    }
-		    case 2:   //For Patient
+		    case 2:   //For Patient   
 		    {
 		    	int flag=0;
 		    	System.out.println("*****************Welcome to patient portal*****************");
@@ -99,7 +92,7 @@ public class Main
 		    	try {
 					Connection con=ConnectionProvider.getCon();
 					Statement st=con.createStatement();
-					st.executeUpdate("Select * from where ");
+					st.executeQuery("Select * from Users where userID=id && userType='Patient' && Password=pd");
 					flag=1;
 				}catch(Exception e){
 					System.out.println("Not Registerd"+e.getMessage());
@@ -108,7 +101,7 @@ public class Main
 		    	{
 		    		while(true)
 		    		{
-		    			System.out.println("1.ViewProfile\n 2.viewDoctors.\n3.BookAppointments\n4.CancelAppointment\n5.PayBill\n6.ViewReport\n7.viewAppointments\n.ChangePassword\n.Logout");
+		    			System.out.println("1.ViewProfile\n2.viewDoctors.\n3.BookAppointments\n4.CancelAppointment\n5.PayBill\n6.ViewReport\n7.viewAppointments\n.ChangePassword\n.Logout");
 		    			int ch=sc.nextInt();
 		    			switch(ch)
 		    			{
@@ -122,32 +115,44 @@ public class Main
 		    				}
 		    				case 3:
 		    				{
-
-		    					pat_app[id].book
-
+		    					
+		    					P[id].BookAppointment(id);
+		    					
 		    				}
 		    				case 4:
 		    				{
-
+		    				
 		    				}
 		    				case 5:
 		    				{
-
+		    					
+		    				}
+		    				case 6:
+		    				{
+		    					
+		    				}
+		    				case 7:
+		    				{
+		    					
+		    				}
+		    				case 8:
+		    				{
+		    					
 		    				}
 		    			}
 		    		}
 		    	}
 		    	break;
-
+		    	
 		    }
 		    case 3: //For Doctor
 		    {
-		    	System.out.println("Welcome to Doctors portal");
+		    	System.out.println("Welcome to Doctor's portal");
 		    	int flag=0;
-		    	String un;
+		    	int id;
 		    	String pd;
 		    	String userType="Doctor";
-		    	System.out.print("USERNAME:");un=sc.next();
+		    	System.out.print("USERNAME:");id=sc.nextInt();
 		    	System.out.print("Password:");pd=sc.next();
 		    	try {
 					Connection con=ConnectionProvider.getCon();
@@ -157,27 +162,43 @@ public class Main
 				}catch(Exception e){
 					System.out.println("Not Registerd");
 				}
+		    	finally {
+		    		System.out.println(" Succesfully Logged In !! ");
+		    	}
 		    	if(flag==1)
 		    	{
 		    		while(true)
 		    		{
-		    			System.out.println("1.View profile\n 2.viewAppointments.\n3.Cancel Appointments\n4.\n5.\n6.\n7.view Appointments\n.8Logout");
+		    			System.out.println("1.viewProfile\n2.viewAppointments.\n3.cancelAppointments\n4Logout");
 		    			int ch=sc.nextInt();
 		    			switch(ch)
 		    			{
 		    				case 1:
 		    				{
-
+		    					D[id].ShowDoctorDetails(id);
+		    				}
+		    				case 2:
+		    				{
+		    					
 		    				}
 		    			}
 		    		}
 		    	}
 		    	break;
-
+		    	
 		    }
-		    case 4:
+		    case 4: 
 		    {
+		    	p=new Patients();
+		    	int pid=p.addPatient();
+		    	System.out.println("Fill the following details");
+		    	p.PatientRegistration(pid);
+		    	
 		    	break;
+		    }
+		    case 5:
+		    {
+		    	
 		    }
 		}
 		}
